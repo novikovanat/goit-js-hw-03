1e5c3291332ef8dd5e4b4f02fd264993c688748b
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-01-04 00:34:54 +0100

    arrays

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,16 @@
-function checkForSpam(message) {
-  const messageInLowerCase = message.toLowerCase();
-  return (
-    messageInLowerCase.includes('spam') || messageInLowerCase.includes('sale')
-  );
+function filterArray(numbers, value) {
+  let numbersBiggerThanValue = [];
+  for (i = 0; i <= numbers.length - 1; i++) {
+    let currentIndexValue = numbers[i];
+    if (value < currentIndexValue) {
+      numbersBiggerThanValue.push(currentIndexValue);
+    }
+  }
+  return numbersBiggerThanValue;
 }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
